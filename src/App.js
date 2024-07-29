54d4792683d5c1fc5a7abed844fdb844da86857e
Alice <maskedwolf211@gmail.con>
2024-07-29 00:55:53 -0700

    updated version of nav

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+const App = () => {
+    const [step , setStep]=useState(1);
+    const [open , setOpen]=useState(true);
+
+function handleOpen(){
+    setOpen(!open);
+    
 }
+// handleOpen();
+
+  function handlePrivious() {
+    if(step>1)
+    setStep(step-1);
+  };
+
+   function handleNext() {
+    if(step<3)
+   setStep(step+1);
+  }
+  
+  const style = { backgroundColor: "#7950f2", color: "#fff" };
+  return (
+  <>
+  <button onClick={handleOpen} className="close">{!open?"x" :"v"}</button>
+  <p>{open&&"we are still working on this project kindly wait"}</p>
+ 
+ {!open&&<div className="steps"> 
+      <div className="numbers">
+        {" "}
+        <div className={step >= 1 ? "active":""}>1</div>
+        <div className={step >= 2 ? "active":""}>2</div>
+        <div className={step >= 3 ? "active":""}>3</div>
+      </div>
+      <p className="message">
+        Step {step}: {messages[step - 1]}{" "}
+      </p>
+      <div className="buttons">
+        <button style={style} onClick={handlePrivious}>
+          previous{" "}
+        </button>
+        <button style={style} onClick={handleNext}>next</button>
+      </div>
+    </div>}
+
+    
+  </>);
+};
 
 export default App;
